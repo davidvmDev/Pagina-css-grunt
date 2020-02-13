38d06ae442e3ca6a6171a5eec0dddb3d1f49783f
davidvmDev <47327684+davidvmDev@users.noreply.github.com>
2020-02-13 18:52:29 -0500

    commit

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/index.js',
+        dest: 'js/index.min.js'
+      }
+    },
+    cssmin:{
+      main:{
+        files:{
+          "css/index.min.css":["css/index.css"]
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+};
